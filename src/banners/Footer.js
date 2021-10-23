cef9a1491d039b926bfc9a7125e80d2a543279fc
Shahidain <mohd.shahidain@gmail.com>
2021-10-23 14:20:57 +0530

    Fixed typos

--- src/banners/Footer.js
+++ src/banners/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                 <label aria-labelledby="footerDiv">&copy;2020 - 2021 BMI</label>
             </div>
             <div style={{textAlign:'center',marginTop:'5px',fontSize:'12px'}}>
-                Verion: 1.0.3.2310211353
+                Version: 1.0.3.2310211353
             </div>
         </>
     )
